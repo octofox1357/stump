297f70c5e478ac26edd78c96b35b03309982f947
octofox1357 <octofox1357@gmail.com>
2023-09-28 17:01:27 +0900

    Feat: 툴팁 기능 추가

--- client/src/routes/epub/+page.svelte
+++ client/src/routes/epub/+page.svelte
@@ -4,6 +4,10 @@
 
 	let book;
 	let rendition;
+	let mousePosition;
+	let tooltip;
+	let iframe;
+	let timeout;
 
 	onMount(() => {
 		book = ePub('/nwt_KO.epub');
@@ -11,6 +15,49 @@
 			spread: 'always',
 			height: 'calc(100vh - 72px)'
 		});
+
+		rendition.on('rendered', function (section) {
+			const iframeElement = document.querySelector('iframe[id^="epubjs-view-"]');
+
+			if (iframeElement instanceof HTMLIFrameElement) {
+				iframe = iframeElement;
+
+				iframe.contentWindow.addEventListener('click', function (event) {
+					// 툴팁이 이미 있는 경우 제거
+					if (tooltip && tooltip.parentNode) {
+						tooltip.parentNode.removeChild(tooltip);
+					}
+				});
+
+				iframe.contentWindow.addEventListener('mousemove', function (event) {
+					clearTimeout(timeout); // 기존의 디바운스 타임아웃을 제거
+					timeout = setTimeout(() => {
+						mousePosition = {
+							x: event.clientX,
+							y: event.clientY
+						};
+						console.log(mousePosition);
+					}, 100); // 100ms 동안 추가 이벤트가 없으면 실행
+				});
+			}
+		});
+
+		rendition.on('selected', function (cfiRange, contents) {
+			tooltip = document.createElement('div');
+			tooltip.innerHTML =
+				'<button id="highlight">Highlight</button><button id="annotate">Annotate</button>';
+			tooltip.style.position = 'fixed';
+			tooltip.style.zIndex = '1000';
+			tooltip.style.background = 'white';
+			tooltip.style.border = '1px solid black';
+
+			tooltip.style.left = `${mousePosition.x}px`;
+			tooltip.style.top = `${mousePosition.y}px`;
+			if (iframe && iframe.contentDocument) {
+				iframe.contentDocument.body.appendChild(tooltip);
+			}
+		});
+
 		rendition.display();
 
 		const next = document.getElementById('next');
